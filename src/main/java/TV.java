9b5575585e802956c0efda0e6a0d1866e6b7d531
janegl2 <78577524+janegl2@users.noreply.github.com>
2021-11-02 23:44:47 -0500

    Update TV.java

--- src/main/java/TV.java
+++ src/main/java/TV.java
@@ -3,7 +3,7 @@ package taller3.televisores;
 public class TV {
 	private Marca marca;
 	private int canal = 1;
-	private int precio = 50;
+	private int precio = 500;
 	private boolean estado;
 	private int volumen = 1;
 	private Control control;
